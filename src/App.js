a13621f9f813a85f1c1118507c794316be401d29
mathew94 <mathewrodrick94@hotmail.com>
2020-08-30 12:33:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
 import './App.css';
+import News from './Components/News/News';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-30&sortBy=publishedAt&apiKey=4550e270fcb04b25a554cdf0dd6dd77a';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Headlines: {articles.length}</h2>
+      {
+        articles.map(article => <News article = {article}></News>)
+      }
     </div>
   );
 }
