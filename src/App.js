2d021e140a8441908b693c0faf5eb8dd92315939
mathew94 <mathewrodrick94@hotmail.com>
2020-08-30 22:30:08 +0600

    axios learning

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
 import './App.css';
 import News from './Components/News/News';
+import axios from '../node_modules/axios';
 
 function App() {
   const [articles, setArticles] = useState([]);
+  // useEffect(() => {
+  //   const url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-30&sortBy=publishedAt&apiKey=4550e270fcb04b25a554cdf0dd6dd77a';
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
+  // }, [])
   useEffect(() => {
     const url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-30&sortBy=publishedAt&apiKey=4550e270fcb04b25a554cdf0dd6dd77a';
-    fetch(url)
-    .then(res => res.json())
-    .then(data => setArticles(data.articles))
-  }, [])
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  } , [])
   return (
     <div>
       <h2>Headlines: {articles.length}</h2>
